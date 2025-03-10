efb51fae5b7acb8b95b9bac7b07f9cbb908e4e71
Thinspeed <offmailno5@gmail.com>
2025-03-10 21:55:14 +0300

    Fix init db sql script. Now new table are available for rw and ro users

--- environments/postgresSQL/init-db/init-user-db.sql
+++ environments/postgresSQL/init-db/init-user-db.sql
@@ -16,20 +16,23 @@ BEGIN
     EXECUTE format('GRANT USAGE, CREATE ON SCHEMA public TO %I', migrator_user);
     EXECUTE format('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO %I', migrator_user);
     EXECUTE format('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO %I', migrator_user);
-    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO %I', migrator_user);
-    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO %I', migrator_user);
+
+    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT ALL ON TABLES TO %I', migrator_user, migrator_user);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT ALL ON SEQUENCES TO %I', migrator_user, migrator_user);
+
 
     EXECUTE format('CREATE USER %I WITH PASSWORD %L', readonly_user, readonly_password);
     EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', current_database(), readonly_user);
     EXECUTE format('GRANT USAGE ON SCHEMA public TO %I', readonly_user);
     EXECUTE format('GRANT SELECT ON ALL TABLES IN SCHEMA public TO %I', readonly_user);
-    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO %I', readonly_user);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT SELECT ON TABLES TO %I', migrator_user, readonly_user);
 
     EXECUTE format('CREATE USER %I WITH PASSWORD %L', readwrite_user, readwrite_password);
     EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', current_database(), readwrite_user);
     EXECUTE format('GRANT USAGE ON SCHEMA public TO %I', readwrite_user);
     EXECUTE format('GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO %I', readwrite_user);
     EXECUTE format('GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO %I', readwrite_user);
-    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I', readwrite_user);
-    EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO %I', readwrite_user);
+
+    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I', migrator_user, readwrite_user);
+    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO %I', migrator_user, readwrite_user);
 END $$;
