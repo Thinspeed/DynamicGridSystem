8c5ba0429f3a27c7693efe322537cd0428d5df7c
Thinspeed <offmailno5@gmail.com>
2025-03-18 22:09:02 +0300

    Update data type

--- EntityFramework.Preferences/Migrations/20250318190832_UpdateDataType.Designer.cs
+++ EntityFramework.Preferences/Migrations/20250318190832_UpdateDataType.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using EntityFramework.Preferences;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EntityFramework.Preferences.Migrations
 {
     [DbContext(typeof(MigrationDbContext))]
-    partial class MigrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250318190832_UpdateDataType")]
+    partial class UpdateDataType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
